2d260ba58e928bf9a76790694265a41570d60fb1
Антон Астафьев <anton@astafiev.me>
2018-12-11 21:27:19 +0700

    импорты

--- src/main/java/me/astafiev/web/compiler/isol/Completer.java
+++ src/main/java/me/astafiev/web/compiler/isol/Completer.java
@@ -10,7 +10,10 @@
 
 package me.astafiev.web.compiler.isol;
 
+import com.sun.source.tree.ImportTree;
+import com.sun.source.tree.MemberSelectTree;
 import com.sun.source.tree.Tree;
+import com.sun.source.tree.Tree.Kind;
 import com.sun.source.tree.VariableTree;
 import com.sun.source.util.JavacTask;
 import com.sun.source.util.TreePath;
@@ -18,7 +21,9 @@ import com.sun.source.util.Trees;
 import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.code.Symtab;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Stream;
+import javax.lang.model.element.Name;
 import me.astafiev.web.compiler.beans.Completion;
 import me.astafiev.web.compiler.beans.CompletionType;
 import net.elliptica.tools.Accessor;
@@ -34,23 +39,33 @@ public class Completer {
 		Tree leaf = treePath.getLeaf();
 		switch (parent.getKind()) {
 			case PARAMETERIZED_TYPE:
-				return allClassTypes(task);
+				return allClassTypes(task, treePath);
 			case VARIABLE:
 				if (((VariableTree)parent).getType() == leaf) {
-					return allClassTypes(task);
+					return allClassTypes(task, treePath);
 				} else break;
 		}
 		return Stream.empty();
 	}
 
-	private Stream<Completion> allClassTypes(JavacTask task) {
+	private Stream<Completion> allClassTypes(JavacTask task, TreePath treePath) {
 		if (classes == null) {
 			classes = new Accessor(task.getTypes()).getField("syms", Symtab.class).classes.values();
+			imports = treePath.getCompilationUnit().getImports();
 		}
-		return classes.stream().map(cl -> cl.getSimpleName().toString())
+
+		Stream<Name> impNames = imports.stream()
+				.map(imp -> imp.getQualifiedIdentifier())
+				.filter(imp -> imp.getKind() == Kind.MEMBER_SELECT)
+				.map(imp -> ((MemberSelectTree)imp).getIdentifier());
+
+		Stream<Name> symNames = classes.stream().map(cl -> cl.getSimpleName());
+
+		return Stream.concat(symNames, impNames).map(Name::toString)
 			.filter(n -> !n.matches(".*\\$[0-9]+.*"))
 			.distinct().map(n -> new Completion(CompletionType.CLASS, n));
 	}
 
 	private Collection<Symbol.ClassSymbol> classes;
+	private List<? extends ImportTree> imports;
 }
